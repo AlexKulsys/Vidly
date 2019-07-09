3f7a57e275094301fb63401dd21c3910c2ffe356
AlexKulsys <52538444+AlexKulsys@users.noreply.github.com>
2019-07-09 13:48:52 +0200

    Add files via upload
    Vidly Controllers

---
+++ CustomersController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose(); // To Dispose object
+        }
+
+        public ActionResult CustomerForm()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+            return View("CustomerForm", viewModel);
+        }
+
+        [HttpPost] // necessary to use this attribute
+        //public ActionResult Save(CustomerFormViewModel viewModel)
+        //public ActionResult Save(Customer customer) //this option can be used to bind data
+        public ActionResult Save(Customer customer, MembershipType type)
+        {
+            /*
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+            return View("New", viewModel);
+            }
+            */
+
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                //TryUpdateModel(customerInDb); //properties of customer will be updated
+
+                //another way to update the fields.
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipType = customer.MembershipType;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+
+
+            }
+
+            _context.SaveChanges(); 
+            
+            return RedirectToAction("Index", "Customers");
+        }
+
+        public ViewResult Index()
+        {
+            //var customers = GetCustomers(); was changed to _context.Customers; 
+            var customers = _context.Customers.Include(c =>c.MembershipType).ToList(); // DbSet define in DbContext and this way we can get all customers in database
+            return View(customers);                   // Include(c =>c.MembershipType) was used to reference customers and to be able to use their info
+
+        }
+        
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+            return View(customer);
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        
+
+        /*private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith"},
+                new Customer { Id = 2, Name = "Mary Williams"}
+            };
+        }
+        */ //not needed after _context.Customers creation
+
+    }
+}
---
+++ MoviesController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models; // to be able to use var movie = new Movie()
+using Vidly.ViewModels; // to use var viewModel = new RandomMovieViewModel // creates view Model object
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+
+namespace Vidly.Controllers
+{
+
+    public class MoviesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Movies/Random
+        /*public ActionResult Random() //Index was changed to Random
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            var customers = new List<Customer> // list of customers
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel // creates view Model object
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel); //for model object movie was changed to viewModel
+
+            //ViewData["Movie"] = movie; // another way to pass data to Views; after this in return view movie can be removed
+            //return View();             // Not suggested to use
+
+
+
+        }
+        //for attribute Routing
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")] //Route Attribute(url template), regex - specifies that only 2 digits
+
+        public ActionResult ByReleaseYear(int year, int month) //action method created for 2019/01 (year/month route)
+        {
+            return Content(year + "/" + month);
+        }
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+        */
+        //Movies
+        public ActionResult Index()
+        {
+            var movies = _context.Movies.Include(m => m.Name).ToList();
+            return View(movies);
+        }
+
+        public ViewResult New()
+        {
+            var genre = _context.Genres.ToList();
+
+            var viewModel = new NewMovieViewModel
+            {
+                Genre = genre
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new NewMovieViewModel
+            {
+                Movie = movie,
+                Genre = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+            return View(movie);
+        }
+
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+
+            }
+
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
+        }
+    }
+}
+
