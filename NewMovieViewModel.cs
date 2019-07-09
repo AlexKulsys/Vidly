dcf9b421f60ee531b03efd16ff9b066e9f01ccc3
AlexKulsys <52538444+AlexKulsys@users.noreply.github.com>
2019-07-09 13:49:17 +0200

    Add files via upload
    Vidly ViewModels

---
+++ NewMovieViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.Migrations;
+
+namespace Vidly.ViewModels
+{
+    public class NewMovieViewModel : Controller
+    {
+        // GET: NewMovieViewModel
+        public IEnumerable <Genre> Genre { get; set; }
+        public Movie Movie { get; set; }
+        public string Title
+        {
+            get
+            {
+                if (Movie != null && Movie.Id != 0)
+                    return "Edit Movie";
+
+                return "New Movie";
+            }
+        }
+    }
+
+}
---
+++ RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models; //Necessary to add to be able to use property - public Movie Movie { get; set; }
+
+namespace Vidly.ViewModels
+{
+    public class RandomMovieViewModel // was created to be able to use View Model
+    {
+        public Movie Movie { get; set; } // neccessary to create Movie property
+        public List<Customer> Customers { get; set; } // List of Customers property
+    }
+}
